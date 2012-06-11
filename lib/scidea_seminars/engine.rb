2dd257762b0e32323d1615f94298bb18f03b853b
Ryan Ahearn <ryan@coshx.com>
2012-06-11 11:16:28 -0400

    PHX-1345 Adds grant form view

--- lib/scidea_seminars/engine.rb
+++ lib/scidea_seminars/engine.rb
@@ -1,5 +1,9 @@
 module ScideaSeminars
   class Engine < ::Rails::Engine
     isolate_namespace ScideaSeminars
+
+    initializer 'scidea.seminars.register_view_callbacks' do
+      Scidea::Plugins::Plugin.on(:admin_seminar_application_form_start) { 'seminar_applications/grant' }
+    end
   end
 end
